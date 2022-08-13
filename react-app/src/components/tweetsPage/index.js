694d0a5aa1cb70344386e0b6d58f0868055a0378
Jack <tranmjack@gmail.com>
2022-08-13 18:56:21 +0000

    fixed delete

--- react-app/src/components/tweetsPage/index.js
+++ react-app/src/components/tweetsPage/index.js
@@ -85,7 +85,7 @@ function TweetsPage() {
                                     <div className="bottomicons">
                               <CreateCommentModal value={tweet.id} />
                               <EditTweetModal tweet={tweet} />
-                              <HiOutlineTrash className="tweetbuttons" size="20px" id={tweet.id} onClick={(e) => handleDeleteClick }/>
+                              <HiOutlineTrash className="tweetbuttons" size="20px" id={tweet.id} onClick={(e) => handleDeleteClick(e) }/>
                               {editTweet && <EditTweetsPage tweetId={tweet.id}/> }</div>
                                </> ) :
                                 <>
